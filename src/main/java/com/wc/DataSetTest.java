7de4c2b9bde483eb6fb6dc8ccbb324f34eff4c54
byebai95 <50128162+byebai95@users.noreply.github.com>
2022-05-15 10:38:29 +0800

    流处理案例

---
+++ src/main/java/com/wc/DataSetTest.java
@@ -0,0 +1,41 @@
+package com.wc;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ *  DataSet 批处理
+ */
+public class DataSetTest {
+    public static void main(String[] args) throws Exception{
+
+        //1.准备本地执行环境
+        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+
+        //2.输入
+        String inputPath = "D:\\work\\flink-demo\\src\\main\\resources\\word.txt";
+        DataSource<String> dataSource = environment.readTextFile(inputPath);
+
+        DataSet<Tuple2<String, Integer>> result = dataSource.flatMap(new MyFlatMap())
+                .groupBy("0")
+                .sum(1);
+        result.print();
+
+
+    }
+
+    static class MyFlatMap implements FlatMapFunction<String,Tuple2<String,Integer>>{
+
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            String[] wordList = s.split(" ");
+            for(String str : wordList){
+                collector.collect(new Tuple2<String,Integer>(str,1));
+            }
+        }
+    }
+}
--- src/main/java/com/wc/DataStreamTest.java
+++ src/main/java/com/wc/DataStreamTest.java
@@ -2,29 +2,35 @@ package com.wc;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
  *  DataSet 批处理
  */
-public class Test01 {
+public class DataStreamTest {
     public static void main(String[] args) throws Exception{
 
         //1.准备本地执行环境
-        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //设置分布式计算的并行度
+        environment.setParallelism(2);
 
         //2.输入
         String inputPath = "D:\\work\\flink-demo\\src\\main\\resources\\word.txt";
-        DataSource<String> dataSource = environment.readTextFile(inputPath);
+        DataStreamSource<String> dataSource = environment.readTextFile(inputPath);
 
-        DataSet<Tuple2<String, Integer>> result = dataSource.flatMap(new MyFlatMap())
-                .groupBy("0")
+        DataStream<Tuple2<String, Integer>> result = dataSource.flatMap(new MyFlatMap())
+                .keyBy("0")
                 .sum(1);
         result.print();
 
+        //3.启动任务
+        environment.execute();
 
     }
 
